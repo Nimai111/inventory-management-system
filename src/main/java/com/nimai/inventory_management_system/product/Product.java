e49d661cbbe94b3b7b22672e0ccb28e95e7045b3
Nimai111 <nimaicharan00000@gmail.com>
2026-09-13 12:14:46 +0530

    feat: add product validation

--- src/main/java/com/nimai/inventory_management_system/product/Product.java
+++ src/main/java/com/nimai/inventory_management_system/product/Product.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +23,32 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Product name is required")
+    @Size(
+            min = 2,
+            max = 100,
+            message = "Product name must be between 2 and 100 characters"
+    )
     private String name;
 
+    @Size(
+            max = 500,
+            message = "Description cannot exceed 500 characters"
+    )
     private String description;
 
+    @NotNull(message = "Price is required")
+    @DecimalMin(
+            value = "0.0",
+            inclusive = false,
+            message = "Price must be greater than 0"
+    )
     private Double price;
 
+    @NotNull(message = "Quantity is required")
+    @Min(
+            value = 0,
+            message = "Quantity cannot be negative"
+    )
     private Integer quantity;
 }
